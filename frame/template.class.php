8649fbd9f9904729b870e6e0180aab93126f46f0
ChrisEsser <esser32@gmail.com>
2017-11-17 11:53:06 -0600

    r

--- frame/template.class.php
+++ frame/template.class.php
@@ -46,6 +46,8 @@ class Template
         // check for a view file matching the action
         if (file_exists($viewsRoot . $this->_controller . DS . $this->_action  . '.php')) {
 
+            echo 'WTF';
+
             // controller global header
             if (file_exists($viewsRoot . $this->_controller . DS . 'header'  . '.php')) {
                 include($viewsRoot . $this->_controller . DS . 'header'  . '.php');
